45d798c4169900eb6aa58c492b9f870fbf551197
yonjanamy-gif <yonjanamy@gmail.com>
2026-06-04 21:08:41 +0545

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,7 @@
+
+print("Half Pyramid Pattern of Stars (*):")
+n = int(input("Enter the number of rows: "))
+for i in range(n):
+    for j in range(i + 1):
+        print("*", end="")
+    print()
---
+++ a3.py
@@ -0,0 +1,27 @@
+rowSize = int(input("enter the number of rows: "))
+if rowSize%2==0: 
+  halfDiamRow = int(rowSize/2)
+else:
+  halfDiamRow = int(rowSize/2)+1
+  
+space = halfDiamRow-1
+for i in range(1, halfDiamRow+1): 
+  for j in range(1, space+1): 
+    print(end=" ")
+  space = space-1
+  num = 1
+  for j in range(2*i-1):
+    print(end=str(num))
+    num = num+1
+  print()
+  
+space = 1
+for i in range(1, halfDiamRow): 
+  for j in range(1, space+1):  
+    print(end=" ")
+  space = space+1
+  num = 1
+  for j in range(1, 2*(halfDiamRow-i)):
+    print(end=str(num)) 
+    num = num+1
+  print()
